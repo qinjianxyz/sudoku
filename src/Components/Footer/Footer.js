d60f43199caf5f19691db836601bc0142e2920eb
Jian Qin <qinjianxyz@Jian-Chin.local>
2021-06-19 15:48:13 -0700

    update

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
 const Footer = () => {
+
+    let d = new Date();
+    let currentYear = d.getFullYear();
+    let display = "Copyright© " + currentYear + ", Jian Qin";
+
     return (
         <div style={{textAlign:'center'}}>
             <hr />
-            <p>Created by Jian Qin, 2021</p>
+            <p>{display}</p>
+            <p>All rights reserved.</p>
             <a href='https://qinjian.xyz'><i>Learn more</i></a>
             <div style={{height:'30px'}}></div>
         </div>
