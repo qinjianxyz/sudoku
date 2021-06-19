78818af81a33a23923a5d3e7dec4bb0d8ca3f7c7
Jian Qin <qinjianxyz@Jian-Chin.local>
2021-06-19 14:28:29 -0700

    update

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import Title from './Components/Title/Title';
 import Board from './Components/Board/Board';
 import Footer from './Components/Footer/Footer'
+import background from './background.png'
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ backgroundImage: `url(${background})` }} >
       <Title />
       <Board />
       <Footer />
